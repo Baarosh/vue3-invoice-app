c40ff4b2e65a3b810f5f2a9a3a79314ac5144e17
Barosh <norbert.lipinski96@gmail.com>
2021-06-21 21:30:40 +0200

    header v1 ready

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <the-header></the-header>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+  name: 'app',
+  components: {
+    TheHeader,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import './variables.scss';
+
+*,
+*::before,
+*::after,
+#app {
+  font-family: 'Poppins', Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  letter-spacing: -1px;
+}
+
+#app {
+  background: -webkit-linear-gradient(180deg, rgb(32, 88, 145), rgb(8, 39, 67));
+  background: linear-gradient(180deg, rgb(32, 88, 145), rgb(8, 39, 67));
+  min-height: 100vh;
+  width: 100%;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,82 @@
+<template>
+  <header class="flex row a-center">
+    <div class="left flex row a-center j-start">
+      <img src="../assets/background_invoice.svg" alt="" />
+      <h1>Invoice Manager</h1>
+      <svg
+        width="232"
+        height="39"
+        viewBox="0 0 232 39"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M4 35C159.893 -1.35364 218.289 1.63977 228 7.68067"
+          stroke="#11EF5C"
+          stroke-width="7"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+    <div class="right flex row a-center j-end">
+      <h4>Sign out</h4>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../variables.scss';
+
+header {
+  height: 120px;
+  width: 100%;
+  .left {
+    position: relative;
+    flex: 1 0 auto;
+    height: 100%;
+
+    img {
+      height: 100px;
+      width: auto;
+      transform: rotate(20deg);
+      margin-left: 20px;
+    }
+
+    h1 {
+      display: inline-block;
+      margin-left: 30px;
+      font-size: 45px;
+      font-weight: bold;
+      color: $white;
+    }
+
+    svg {
+      position: absolute;
+      top: 85px;
+      left: 320px;
+    }
+  }
+
+  .right {
+    flex: 1 0 auto;
+    height: 100%;
+    h4 {
+      display: inline-block;
+      margin-right: 30px;
+      font-size: 25px;
+      font-weight: bold;
+      color: $white;
+    }
+  }
+}
+
+svg {
+}
+</style>
