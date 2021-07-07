7f6c16edbbab9e016289a21df561626e1cc73a16
Baarosh <norbert.lipinski96@gmail.com>
2021-07-07 21:45:18 +0200

    added form validation v1
    added form validation v1

--- src/components/AddInvoice.vue
+++ src/components/AddInvoice.vue
@@ -1,39 +1,47 @@
 <template>
-  <div class="section3-wrapper" @click="ToggleModalOff" ref="wrapper">
+  <div class="section3-wrapper" @click="toggleModalOff" ref="wrapper">
     <section class="section3 flex column a-center j-center">
       <form class="flex column a-start j-start">
         <h2>New Invoice</h2>
         <div class="flex row a-center j-start">
           <label for="invoiceId">Invoice Id</label>
-          <input type="text" id="invoiceId" disabled value="#4325" ref="invoiceId" />
+          <input type="text" id="invoiceId" disabled value="4325" ref="invoiceId" />
         </div>
         <div class="flex row a-center j-start">
           <label for="date">Date</label>
-          <input type="date" id="date" ref="date" />
+          <input type="date" id="date" ref="date" @input="validateAttribute('date')" />
+          <p class="invalid" v-if="dateValid === 0">Value is invalid, please correct!</p>
+          <p class="valid" v-if="dateValid === 1">Value is valid!</p>
         </div>
         <div class="flex row a-center j-start">
           <label for="country">Country</label>
-          <select id="country" ref="country">
+          <select id="country" ref="country" @input="validateAttribute('country')">
             <option value="Country" selected disabled>*Select country*</option>
             <option value="France">France</option>
             <option value="Poland">Poland</option>
             <option value="Spain">Spain</option>
           </select>
+          <p class="invalid" v-if="countryValid === 0">Value is invalid, please correct!</p>
+          <p class="valid" v-if="countryValid === 1">Value is valid!</p>
         </div>
         <div class="flex row a-center j-start">
           <label for="zipCode">Zip Code</label>
-          <input type="text" id="zipCode" ref="zipCode" />
+          <input type="text" id="zipCode" ref="zipCode" @input="validateAttribute('zipCode')" />
+          <p class="invalid" v-if="zipCodeValid === 0">Value is invalid, please correct!</p>
+          <p class="valid" v-if="zipCodeValid === 1">Value is valid!</p>
         </div>
         <div class="flex row a-center j-start">
           <label for="total">Total</label>
-          <input type="number" id="total" ref="total" />
+          <input type="number" id="total" ref="total" @input="validateAttribute('total')" />
+          <p class="invalid" v-if="totalValid === 0">Value is invalid, please correct!</p>
+          <p class="valid" v-if="totalValid === 1">Value is valid!</p>
         </div>
         <div class="buttons flex row a-center j-start">
           <button type="button" class="btn btn-cancel" @click="TOGGLE_MODAL_OFF">Cancel</button>
-          <button type="button" class="btn btn-draft" @click="ADD_INVOICE('Draft')">
+          <button type="button" class="btn btn-draft" @click="createInvoice('Draft')">
             Save as draft
           </button>
-          <button type="button" class="btn btn-create" @click="ADD_INVOICE('Pending')">
+          <button type="button" class="btn btn-create" @click="createInvoice('Pending')">
             Create invoice
           </button>
         </div>
@@ -46,13 +54,18 @@
 export default {
   name: 'AddInvoice',
   data() {
-    return {};
+    return {
+      dateValid: null,
+      countryValid: null,
+      zipCodeValid: null,
+      totalValid: null,
+    };
   },
   methods: {
     TOGGLE_MODAL_OFF() {
       this.$store.dispatch('TOGGLE_MODAL_OFF');
     },
-    ToggleModalOff(event) {
+    toggleModalOff(event) {
       if (event.target === this.$refs.wrapper) {
         this.$store.dispatch('TOGGLE_MODAL_OFF');
       }
@@ -73,6 +86,51 @@ export default {
           status,
         });
         this.$store.dispatch('TOGGLE_MODAL_OFF');
+      }
+    },
+    validateAttribute(attribute) {
+      if (attribute === 'date') {
+        const d = new Date(this.$refs.date.value);
+        const day = d.getDate();
+        const month = d.getMonth();
+        const year = d.getFullYear(0);
+        if (day >= 1 && day <= 31 && month >= 1 && month <= 12 && year >= 1901 && year <= 2100) {
+          this.dateValid = 1;
+        } else {
+          this.dateValid = 0;
+        }
+      } else if (attribute === 'country') {
+        if (this.$refs.country.value !== 'Country') {
+          this.countryValid = 1;
+        } else {
+          this.countryValid = 0;
+        }
+      } else if (attribute === 'zipCode') {
+        if (this.$refs.zipCode.value.length >= 4 && this.$refs.zipCode.value.length <= 10) {
+          this.zipCodeValid = 1;
+        } else {
+          this.zipCodeValid = 0;
+        }
+      } else if (
+        this.$refs.total.value >= -100000 &&
+        this.$refs.total.value <= 100000 &&
+        this.$refs.total.value.length > 0
+      ) {
+        this.totalValid = 1;
+      } else {
+        this.totalValid = 0;
+      }
+    },
+    validateForm() {
+      return this.dateValid && this.countryValid && this.zipCodeValid && this.totalValid;
+    },
+    createInvoice(status) {
+      this.validateAttribute('date');
+      this.validateAttribute('country');
+      this.validateAttribute('zipCode');
+      this.validateAttribute('total');
+      if (this.validateForm()) {
+        this.ADD_INVOICE(status);
       }
     },
   },
@@ -105,6 +163,7 @@ export default {
   form {
     height: 90%;
     width: 90%;
+
     h2 {
       font-size: 36px;
       color: $white;
@@ -126,8 +185,16 @@ export default {
         width: 200px;
         padding: 2px 5px;
       }
+      p {
+        margin-left: 15px;
+      }
+      .valid {
+        color: $green-validation;
+      }
+      .invalid {
+        color: $red-validation;
+      }
     }
-
     .buttons {
       margin-top: 230px;
       width: 100%;
